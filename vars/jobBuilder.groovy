5a18f3e13db8c8d7915e2da0509592a55b220cfd
delhijalboard <delhijalboard25@gmail.com>
2026-02-23 14:54:46 +0530

    Update jobBuilder.groovy

--- vars/jobBuilder.groovy
+++ vars/jobBuilder.groovy
@@ -12,7 +12,7 @@ metadata:
 spec:
   containers:
   - name: build-utils
-    image: egovio/build-utils:7-master-95e76687
+    image: docker.io/egovio/build-utils:7-master-95e76687
     imagePullPolicy: IfNotPresent
     command:
     - cat
